de579375eecf6d8ae9dcac451f920346391f436f
ntthanhvy <nguyenthithanhvyjps15@gmail.com>
2022-12-02 10:28:50 +0700

    mini-apps: update ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	base: "./",
-	build: {
-		modulePreload: {
-			polyfill: false,
-		},
-		rollupOptions: {
-			output: {
-				entryFileNames: "assets/[name].[hash].module.js",
-				chunkFileNames: "assets/[name].[hash].module.js",
-			},
-		},
-	},
+	build: {},
 });
